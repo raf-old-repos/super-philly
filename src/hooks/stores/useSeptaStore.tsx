ece35cd1795ae2c477d5ec60651be08d53e69472
Gitter499 <rafayel.amirkhanyan@gmail.com>
2022-01-11 16:31:40 +0000

    quick commit

--- src/hooks/stores/useSeptaStore.tsx
+++ src/hooks/stores/useSeptaStore.tsx
@@ -1,9 +1,41 @@
 import create from "zustand";
 
-const REGIONAL_RAIL_STATIONS = [
-    
-]
+import { REGIONAL_RAIL_STATIONS } from "../../data/constants";
 
-const useCovidStore = create((set) => ({}));
 
-export default useCovidStore;
+type RegionalRailTrain = {
+    direction: string | null,
+    path: string | null,
+    train_id: string | null,
+    origin: string | null,
+    destination: string | null,
+    line: string | null,
+    status: string | null,
+    service_type: "LOCAL" | "EXPRESS" | null,
+    next_station: string | null,
+    sched_time: string | Date | null,
+    depart_time: string | Date | null,
+    track: string | null,
+    track_change: string | null,
+    platform: string | null,
+    platform_change: string | null
+}
+
+type BiDirectionalData = {
+    Northbound: RegionalRailTrain[],
+    Southbound: RegionalRailTrain[]
+}
+type RegionalRailArrivalsDeparturesType = {
+    [stationHeader: string]: BiDirectionalData[]
+}
+
+const septaBaseURL = "http://www3.septa.org/hackathon/Arrivals/"
+
+type useRegionalRailStoreType = {
+    train: RegionalRailTrain,
+    getTrainByName: 
+}
+
+export const useRegionalRailStore = create((set) => ({
+
+}));
