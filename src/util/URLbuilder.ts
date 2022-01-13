eb279c97c1178296d439b809ce6262f53f90a165
Gitter499 <rafayel.amirkhanyan@gmail.com>
2022-01-13 15:56:46 +0000

    Fixed minor brain fart

--- src/util/URLbuilder.ts
+++ src/util/URLbuilder.ts
@@ -1,26 +1,26 @@
 export class URLbuilder {
-    public base?: string
-    private finalUrl: string = ""
+    public base: string
+    private addend: string = ""
     constructor(base: string) {
         this.base = base.endsWith("/") ? base : base.concat("/")
     }
 
-    public attach(route: string) {
-        this.finalUrl = this.finalUrl.concat(`${route}/`)
+    public attach(route: string): URLbuilder {
+        this.addend = this.addend.concat(`${route}/`)
         return this
     }
 
-    public param(key: string, val: string) {
-        if (this.finalUrl.endsWith("/")) {
-            const newUrl = this.finalUrl.substring(0, this.finalUrl.length - 1)
-            this.finalUrl = newUrl.concat(`?${key}=${val}`)
+    public param(key: string, val: string): URLbuilder {
+        if (this.addend.endsWith("/")) {
+            const newUrl = this.addend.substring(0, this.addend.length - 1)
+            this.addend = newUrl.concat(`?${key}=${val}`)
         } else {
-            this.finalUrl = this.finalUrl.concat(`?${key}=${val}`)
+            this.addend = this.addend.concat(`?${key}=${val}`)
         }
         return this
     }
-    public url() {
-        return this.finalUrl
+    public url(): string {
+        return this.base.concat(this.addend)
     }
 
 }
